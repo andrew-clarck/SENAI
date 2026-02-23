cc0b09b5ca51e09b66cef16afea768d88a11b263
Andrew Clarck <andrew.clarck@aluno.senai.br>
2026-02-23 12:13:07 -0300

    Aula 2302 - Mobile - Aula 03 - JSX e Estrutura

--- SENAI/MOBILE/exemplos/projeto01/first-app/src/App.js
+++ SENAI/MOBILE/exemplos/projeto01/first-app/src/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Exemplo01 from './jsx_examples/exemplo-01-estrutura';
+import Exemplo02 from './jsx_examples/exemplo-02-expressoes';
+import Exemplo03 from './jsx_examples/exemplo-03-condicionais';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Exemplo03 />
     </View>
   );
 }
@@ -13,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f5f5f5',
     alignItems: 'center',
     justifyContent: 'center',
   },
