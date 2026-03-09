6a479415b6e595b0c01ecd95acda5e932b782de9
Andrew Clarck <andrew.clarck@aluno.senai.br>
2026-03-09 15:56:07 -0300

    Aula 0903 - Mobile - Flexbox

--- SENAI/MOBILE/exemplos/projeto01/first-app/src/App.js
+++ SENAI/MOBILE/exemplos/projeto01/first-app/src/App.js
@@ -1,17 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Exemplo01 from './jsx_examples/exemplo-01-estrutura';
-import Exemplo02 from './jsx_examples/exemplo-02-expressoes';
-import Exemplo03 from './jsx_examples/exemplo-03-condicionais';
-import Exemplo04 from './jsx_examples/exemplo-04-listas';
-import Lista01 from './jsx_exercises/lista01_andrewclarck';
-import Lista02 from './jsx_exercises/lista02_andrewclarck';
-import Lista03 from './jsx_exercises/lista03_andrewclarck';
+import ViewExample from './basic_components/view_examples01';
+import ViewExample02 from './basic_components/view_examples02';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Lista02 />
+      <ViewExample02 />
     </View>
   );
 }
