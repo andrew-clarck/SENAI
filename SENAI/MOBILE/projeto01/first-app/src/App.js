bbbc94803d4fa927e94c577f5fcb5c7cd993394a
Andrew Clarck <andrew.clarck@aluno.senai.br>
2026-05-25 12:20:13 -0300

    Aula 2505 - MOBILE - Navegação exercicio

--- SENAI/MOBILE/projeto01/first-app/src/App.js
+++ SENAI/MOBILE/projeto01/first-app/src/App.js
@@ -2,11 +2,13 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import StackNavigator from "./navigation/exemplos/stack_navigator";
+import BottomTabNavigator from "./navigation/exemplos/bottom_tab_navigator";
+import DrawerNavigator from "./navigation/exemplos/drawer_navigator";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <StackNavigator />
+      <DrawerNavigator />
     </NavigationContainer>
   );
 }
