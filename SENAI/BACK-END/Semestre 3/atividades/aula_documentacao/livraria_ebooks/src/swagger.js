70b2e3a98b6825dea9f1fcce8c3bdcc0b3394c1f
Andrew Clarck <andrew.clarck@aluno.senai.br>
2026-09-24 09:50:48 -0300

    Aula 2409 - BACK - Atividade e Somativa

---
+++ SENAI/BACK-END/Semestre 3/atividades/aula_documentacao/livraria_ebooks/src/swagger.js
@@ -0,0 +1,18 @@
+const swaggerAutogen = require("swagger-autogen");
+
+const doc = {
+  info: {
+    title: "API Livraria",
+    description: "Documentação da API",
+    version: "1.0.0",
+  },
+  host: "localhost:3000",
+  schemes: ["http"],
+};
+
+const outputFile = "./swagger_output.json";
+const endpointsFiles = ["./src/routes/index.js"];
+
+swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
+  console.log("Documentação do Swagger gerada com sucesso!");
+});
